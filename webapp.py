7b55cd78ee7f9cce86450fdc6c7f5f2e0105bb9b
VIRANIDEV <161119496+VIRANIDEV@users.noreply.github.com>
2024-02-29 00:35:01 +0530

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -33,7 +33,7 @@ if choice=="Home":
     st.subheader("Welcome To Chronic kidney disease (CKD) Prediction System")
     st.image('Home.jpg')
     st.subheader('Chronic kidney Disease(CKD)')
-    st.str('CKD.text')
+    st.text('CKD.text')
 
 if choice=="Login":
     Email = st.sidebar.text_input("Email")
