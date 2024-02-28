6e2ce1a85a37762d38c6c3f2b95bd3496c5764b3
VIRANIDEV <161119496+VIRANIDEV@users.noreply.github.com>
2024-02-29 02:20:28 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -43,7 +43,6 @@ if choice=="Home":
     "These classification models achieves reasonable accuracy in Chronic Kidney Disease prediction."
     )
 
-    st.image('image.jpg')
 
 if choice=="Login":
     Email = st.sidebar.text_input("Email")
